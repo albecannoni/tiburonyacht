e32bcbbb85cbb59fadc538ded5995b1e0c3856fd
albecannoni <alberto.cannoni@outlook.it>
2022-10-20 08:36:34 +0200

    update form

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,25 @@
+let form = document.querySelector('#formRichiesta');
+
+
+
 //*innesco init
 window.addEventListener('load', init);
 
 function init() {
-   console.log('test');    
+   console.log('test'); 
+   
+      
+}
+
+function fillForm (){
+   let adrMail = document.querySelector('#formRichiesta');
+   adrMail.setAttribute("action", 'mailto:alberto.cannoni@outlook.it');
+   form.setAttribute('href', 'mailto:a.cannoni@hotmail.com')
+   form.setAttribute("action", "mailto:a.cannoni@hotmail.com");
+}
+
+function fillWapp() {   
+   btnHrefTel.setAttribute("href", "https://wa.me/393890172024");
+   
+  
 }
